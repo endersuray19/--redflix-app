0888b5f770d364c1a7f49620669cd01fe5f68e90
endersurya <endersurya19@gmail.com>
2025-02-19 13:00:09 +0800

    NextAuth, Prisma and Mongodb Setup

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,16 +1,43 @@
-
-import Input from "@/component/Input";
+import axios from 'axios';
+import Input from "@/pages/component/Input";
 import { useCallback, useState } from "react";
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Auth = ()=>{
+    const router = useRouter();
     const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
+    const [name, setName] = useState('');
     const [password, setPassword] = useState('');
 
     const[variant, setVariant] = useState("login");
     const toggleVariant = useCallback(()=>{
         setVariant((currentVariant)=>currentVariant ==='login'?'register':'login')
-    },[])
+    },[]);
+     const login = useCallback(async()=>{
+        try{
+            await signIn('credentials',{
+                email, password,redirect:false,callbackUrl:'/'
+            })
+            router.push('/');
+        }
+        catch(error){
+            console.log(error);
+        }
+    },[email,password])
+    const register = useCallback(async()=>{
+        try{
+            await axios.post("/api/register",{
+                email,
+                name,
+                password
+            });
+            login();
+        }catch(error){
+            console.log(error);
+        }
+    },[email, name, password,login]);
+   
     return(
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-cover bg-no-repeat bg-fixed bg-center]">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -24,13 +51,13 @@ const Auth = ()=>{
                         </h2>
                         <div className="flex flex-col gap-4">
                             {variant === 'register' && (
-                                <Input label="Email" onChange={(ev:any)=>setEmail(ev.target.value)} id="email" type="email" value=""/>
+                               <Input label="Name" onChange={(ev:any)=>setName(ev.target.value)} id="name" type="text" value={name}/>
                             )}
-                            
-                             <Input label="Username" onChange={(ev:any)=>setUsername(ev.target.value)} id="username" type="username" value=""/>
-                              <Input label="Password" onChange={(ev:any)=>setPassword(ev.target.value)} id="password" type="password" value=""/>
+                             <Input label="Email" onChange={(ev:any)=>setEmail(ev.target.value)} id="email" type="email" value={email}/>
+                             
+                              <Input label="Password" onChange={(ev:any)=>setPassword(ev.target.value)} id="password" type="password" value={password}/>
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === 'login' ? 'Login': 'Sign up'}</button>
+                        <button onClick={variant === 'login' ?login :register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === 'login' ? 'Login': 'Sign up'}</button>
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'First time using Redflix ?' :'Already have an account'}
                         
--- pages/component/Input.tsx
+++ pages/component/Input.tsx
@@ -4,7 +4,7 @@ interface inputProps{
     onChange:any; 
     value:string;
     label:string;
-    type:string;
+    type?:string;
 }
 const Input: React.FC<inputProps>= ({
     id,
@@ -16,7 +16,7 @@ const Input: React.FC<inputProps>= ({
  return (
         <div className="relative">
              <input value={value} type={type} onChange={onChange} id={id} className="block rounded-md px-6 pt-6 pb-2 w-full text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer"  placeholder=" "/>
-             <label htmlFor={id} className="absolute text-md text-zinc-400 duration-300 transform top-5 left-6 origin-[0] scale-100 peer-placeholder-shown:top-5 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:scale-75">{label}</label>
+             <label htmlFor={id} className="absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 left-6 origin-[0]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:translate-y-3 peer-focus:scale-75">{label}</label>
         </div>
        
     )
